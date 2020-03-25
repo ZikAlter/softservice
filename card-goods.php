9d62325406eeb7ea3e6e06081ca58fe27e64811c
Адикаев Никита Станиславович <Nikita.Adikaev@main.russianpost.ru>
2020-03-25 14:38:56 +0700

    Update full project

--- card-goods.php
+++ card-goods.php
@@ -1,5 +1,15 @@
 <?php
 require_once('php/connect.php'); // соединение с БД
+$num_str = (int) $_GET['id']; // полученный id является числом
+$num_str = trim($num_str); // удаляем лишние пробелы
+$num_str = strip_tags($num_str); // удаляем html теги
+$num_str = stripslashes($num_str); // удаляем экранирование
+$num_str = htmlspecialchars($num_str); // преобразуем html теги в символы
+
+$stmt = $con_li->prepare( "SELECT * FROM programm WHERE id = ?" ); // подготавливаем наш запрос
+$stmt->bind_param( "i", $num_str ); // подготовка переменных
+$stmt->execute(); // выполняем подготовленный запрос
+$result = $stmt->get_result(); // получаем результат из подготовленного запроса
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -16,8 +26,6 @@ require_once('php/connect.php'); // соединение с БД
   	<link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
 	<!-- исправление стилей в некоторых браузерах -->
 	<link rel="stylesheet" href="css/normalize.css">
-	<!-- css анимация для работы wow JS -->
-	<link rel="stylesheet" href="css/animate.css">
 	<!-- css файл -->
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<!-- библиотека jQuery для работы плагинов и функционала на стороне клиента -->
@@ -34,42 +42,46 @@ require_once('php/connect.php'); // соединение с БД
 		<ul class="nav-menu-cont">
 			<li class="nav-menu-cont-item"><a href="index.html">О нас</a></li>
 			<li class="nav-menu-cont-item"><a href="news.html">Новости</a></li>
-			<li class="nav-menu-cont-item"><a href="soft.php" class="active">Софт и ПО</a></li>
+			<li class="nav-menu-cont-item"><a href="soft.php">Софт и ПО</a></li>
 			<li class="nav-menu-cont-item"><a href="vacancy.html">Вакансии</a></li>
 			<li class="nav-menu-cont-item"><a href="contact.html">Контакты</a></li>
 		</ul>
 	</nav>
 </header>
 <main>
-	<section class="block-goods container wow fadeIn" data-wow-duration="2s">
-		<?php
-        $load_goods = mysqli_query($con_li, "SELECT `id` , `title_programm` , `price` , `img_programm` FROM `programm` ORDER BY(`id`) DESC"); // sql запрос 
-        $while_goods = mysqli_num_rows($load_goods); // подсчет числа товаров
-        if ($while_goods > 0) { // если кол-во товара больше нуля
-          while($info_card = mysqli_fetch_assoc($load_goods)){ // формируем массив для вывода товара
-        ?>
-		<div class="block-goods-card">
-			<img src="files/<?php echo $info_card['img_programm'];?>" alt="" class="goods-card-img">
-			<div class="block-goods-card-title">
-				<h3><?php echo $info_card['title_programm'];?></h3>
-			</div>
-			<div class="block-goods-card-foot">
-				<p class="goods-card-price"><?php echo $info_card['price'];?> <i class="fas fa-ruble-sign"></i></p>
-				<a href="/card-goods.php?id=<?php echo $info_card['id'];?>">
-					<button class="goods-card-button">Подробнее</button>
-				</a>
-			</div>	
-		</div>
-		<?php
-          }
-        }
-        else{ // если ничего нет то выводим сообщение
-        ?>
-        <h2>Список товаров не найден!</h2>
-        <?php
-        } 
-        ?>
-	</section>
+	<?php
+    if(mysqli_num_rows($result) <= 0 ){ // если страница не найдена выводим сообщение что контент не найден
+    ?>
+    	<div class="container">
+    		<h1>Ошибка! Контент не найден...</h1>
+    	</div>
+    <?php
+    }
+    else{ // если страница найдена
+      $info_prog = mysqli_fetch_assoc($result); // присваиваем информацию в переменную
+    ?>
+    	<section class="block-softprogramm container">
+    		<h2 class="block-softprogramm-h2"><?php echo $info_prog['title_programm']; ?></h2>
+    		<div class="block-softprogramm-card">
+    			<div class="block-softprogramm-card-left"><img src="files/<?php echo $info_prog['img_programm']; ?>" alt="" class="card-left-img"></div>
+    			<div class="block-softprogramm-card-right">
+    				<h2 class="card-right-h2">Цена: <?php echo $info_prog['price']; ?><i class="fas fa-ruble-sign"></i></h2>
+    				<p class="col-mvideo"><b>Товар в наличии</b></p>
+    				<h3>Характеристики</h3>
+    				<p><b class="bord-dotted">Тип программы:</b> <?php echo $info_prog['type_programm']; ?></p>
+    				<p><b class="bord-dotted">Производитель:</b> <?php echo $info_prog['manufacturer']; ?></p>
+    				<p><b class="bord-dotted">Срок лицензии:</b> <?php echo $info_prog['license_term']; ?></p>
+    				<p><b class="bord-dotted">Совместимость с ОС:</b> <?php echo $info_prog['compatibility']; ?></p>
+    				<p><b class="bord-dotted">Комплект:</b> <?php echo $info_prog['set_programm']; ?></p>
+    			</div>
+    		</div>
+    		<div class="block-descript-programm">
+    			<p><?php echo $info_prog['description']; ?></p>
+    		</div>
+    	</section>
+    <?php
+	}
+    ?>
 </main>
 <footer>
 	<div class="footer-block container">
@@ -86,13 +98,5 @@ require_once('php/connect.php'); // соединение с БД
 		</div>
 	</div>
 </footer>
-<script src="js/auto-height.js"></script>
-<script type="text/javascript">
-	GreatBalancer(".block-goods-card",".block-goods-card-title",".block-goods-card-foot");
-</script>
-<script src="js/wow.min.js"></script>
-<script>
-new WOW().init();
-</script>
 </body>
 </html>
